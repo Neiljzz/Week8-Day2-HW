d25a4e603f7387aa4f19af1a5a1c0a5a0b296984
Neiljzz <neiljzzzzzz@gmail.com>
2023-04-05 12:19:02 -0600

    second commit

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/NavBar';
+import Pokemon from './views/Pokemon';
+import ToDo from './views/ToDo';
+
+export default function App() {
+    return (
+        <>
+            <Navbar />
+            <div className='container'>
+                <Routes>
+                    <Route path='/' element={<ToDo />} />
+                    <Route path='/pokemon' element={<Pokemon />} />
+                </Routes>
+            </div>
+        </>
+    )
+}
